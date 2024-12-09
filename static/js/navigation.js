6d55ced31ea15ae018bbfa0fc9a7f36e2203bb50
Armand CAMPONOVO <contact@camarm.dev>
2024-12-09 17:52:46 +0100

    copied ressources to static folder

---
+++ static/js/navigation.js
@@ -0,0 +1,22 @@
+// Ressources to switch pages with nice transition
+
+const loader = document.getElementById('loader')
+
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        loader.classList.add('hidden')
+    }, 1000)
+})
+
+document.addEventListener('beforeunload', () => {
+    loader.classList.remove('hidden')
+})
+
+function goTo(path, gameImage = 'img/arcade.png') {
+    loader.classList.remove('hidden')
+    const loaderImage = loader.querySelector('img')
+    loaderImage.src = gameImage
+    setTimeout(() => {
+        location.href = path
+    }, 500)
+}
