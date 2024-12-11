c05c3494264f9ac498d654156fce285d238d1580
Armand Camponovo <contact@camarm.dev>
2024-12-11 09:50:01 +0000

    redirection vers les templates des jeux et propriété logged in

--- main.py
+++ main.py
@@ -5,11 +5,24 @@ app = Flask('Site de minijeux')
 
 @app.get('/')
 def home():
-  return render_template('index.html', pseudo='invité')
+  return render_template('index.html', pseudo='invité', logged_in=False)
 
 @app.get('/game/dino')
 def dino():
-  return render_template('dino.html', pseudo='invité')
+  return render_template('dino.html', pseudo='invité', logged_in=False)
+
+@app.get('/game/osu')
+def dino():
+  return render_template('osu.html', pseudo='invité', logged_in=False)
+
+@app.get('/game/morpion')
+def dino():
+  return render_template('morpion.html', pseudo='invité', logged_in=False)
+
+@app.get('/game/justeprix')
+def dino():
+  return render_template('justeprix.html', pseudo='invité', logged_in=False)
+
 
 if __name__ == '__main__':
   database = sqlite3.connect('database.db', check_same_thread=False)
